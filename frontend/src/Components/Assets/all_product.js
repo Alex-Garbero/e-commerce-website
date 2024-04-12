6eda6aed96e7e7f826d2f92eb91b376c47ee9ab4
alex <144194651+Alex-Garbero@users.noreply.github.com>
2024-04-12 12:45:41 -0700

    Created all_product.js for product image base

---
+++ frontend/src/Components/Assets/all_product.js
@@ -0,0 +1,111 @@
+import img1 from "./img1.png";
+import img2 from "./img2.png";
+import img3 from "./img3.png";
+import img4 from "./img4.png";
+import img5 from "./img5.png";
+import img6 from "./img6.png";
+import img7 from "./img7.png";
+import img8 from "./img8.png";
+import img9 from "./img9.png";
+import img10 from "./img10.png";
+import img11 from "./img11.png";
+import img12 from "./img12.png";
+import img13 from "./img13.png";
+import img14 from "./img14.png";
+import img15 from "./img15.png";
+import img16 from "./img16.png";
+import img17 from "./img17.png";
+import img18 from "./img18.png";
+import img19 from "./img19.png";
+import img20 from "./img20.png";
+import img21 from "./img21.png";
+import img22 from "./img22.png";
+import img23 from "./img23.png";
+import img24 from "./img24.png";
+import img25 from "./img25.png";
+import img26 from "./img26.png";
+import img27 from "./img27.png";
+import img28 from "./img28.png";
+import img29 from "./img29.png";
+import img30 from "./img30.png";
+import img31 from "./img31.png";
+import img32 from "./img32.png";
+import img33 from "./img33.png";
+import img34 from "./img34.png";
+import img35 from "./img35.png";
+import img36 from "./img36.png";
+
+let all_product = [
+    {
+        id: 1,
+        name: "Legend Series Lined Athletic Shorts | Hyper Black",
+        category: "bottoms",
+        image: img1,
+        new_price: 40.0,
+        old_price: 50.0,
+    },
+    {
+        id: 2,
+        name: "Legend Series Lined Athletic Shorts | Desert Sand",
+        category: "bottoms",
+        image: img2,
+        new_price: 40.0,
+        old_price: 50.0,
+    },
+    {
+        id: 3,
+        name: "Stealth Series Lined Athletic Shorts | Concrete Grey",
+        category: "bottoms",
+        image: img3,
+        new_price: 40.0,
+        old_price: 50.0,
+    },
+    {
+        id: 4,
+        name: "Summer Sun Tropics Shorts | Tan",
+        category: "bottoms",
+        image: img4,
+        new_price: 40.0,
+        old_price: 50.0,
+    },
+    {
+        id: 5,
+        name: "Legend Series Athletic Joggers | Overguard Blue",
+        category: "bottoms",
+        image: img5,
+        new_price: 60.0,
+        old_price: 80.0,
+    },
+    {
+        id: 6,
+        name: "The Daily Pant | Charcoal Grey",
+        category: "bottoms",
+        image: img6,
+        new_price: 60.0,
+        old_price: 80.0,
+    },
+    {
+        id: 7,
+        name: "The Endgame Chino | Machiavelli Brown",
+        category: "bottoms",
+        image: img6,
+        new_price: 60.0,
+        old_price: 80.0,
+    },
+    {
+        id: 8,
+        name: "First Choice Jogger | Heather Grey",
+        category: "bottoms",
+        image: img8,
+        new_price: 50.0,
+        old_price: 70.0,
+    },
+    {
+        id: 8,
+        name: "The Stance Pant | Hunter Green",
+        category: "bottoms",
+        image: img8,
+        new_price: 50.0,
+        old_price: 70.0,
+    },
+]
