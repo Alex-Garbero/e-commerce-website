500e1239bcc3ae00a1a30c817112a14b4ac5cfa2
alex <144194651+Alex-Garbero@users.noreply.github.com>
2024-04-17 12:08:48 -0700

    Changed website title and uploaded pngs

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import './Navbar.css'
+import logo from '../Assets/logo.png'
+import cart_icon from '../Assets/cart_icon.png'
+
 export const Navbar = () => {
   return (
     <div className='navbar'>
-        
+      <div className='nav-logo'>
+        <img src={logo} alt="" />
+        <p>SOMPLANEY</p>
+      </div>
+      <ul className='nav-menu'>
+        <li>Shop</li>
+        <li>Bottoms</li>
+        <li>Tops</li>
+        <li>Accessories</li>
+      </ul>
+      <div className="nav-login-cart">
+        <button>Login</button>
+        <img src={cart-icon} alt="" />
+      </div>
     </div>
   )
 }
