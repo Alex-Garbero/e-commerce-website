f75dba72d5a423c0df11bd0dfaf0b892f09024f0
Alex Garbero <144194651+Alex-Garbero@users.noreply.github.com>
2024-04-19 11:58:33 -0700

    Uploaded new pngs for shop page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,30 @@
 import './App.css';
 import { Navbar } from './Components/Navbar/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Shop } from './Pages/Shop';
+import { Cart } from './Pages/Cart';
+import { ShopCategory } from './Pages/ShopCategory';
+import { Product } from './Pages/Product';
+import { LoginSignup } from './Pages/LoginSignup';
 
 function App() {
   return (
     <div>
+      <BrowserRouter>
       <Navbar/>
+      <Routes>
+        {/* Based on the browser path --> set page to element */}
+        <Route path='/' element={<Shop/>}/>
+        <Route path='/bottoms' element={<ShopCategory category='bottoms'/>}/>
+        <Route path='/tops' element={<ShopCategory category='tops'/>}/>
+        <Route path='/accessories' element={<ShopCategory category='accessories'/>}/>
+        <Route path='/product' element={<Product/>}>
+          <Route path=':productId' element={<Product/>}/>
+        </Route>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/login' element={<LoginSignup/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -4,6 +4,7 @@ import './Navbar.css'
 
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
+import { Link } from 'react-router-dom'
 
 export const Navbar = () => {
     // Use state variable to change line for selected page
@@ -17,14 +18,15 @@ export const Navbar = () => {
       </div>
       <ul className='nav-menu'>
         {/* Using ternary operator to check menu selection */}
-        <li onClick={()=>{setMenu("shop")}}>Shop {menu==="shop"?<hr/>:<></>}</li>
-        <li onClick={()=>{setMenu("bottoms")}}>Bottoms {menu==="bottoms"?<hr/>:<></>}</li>
-        <li onClick={()=>{setMenu("tops")}}>Tops {menu==="tops"?<hr/>:<></>}</li>
-        <li onClick={()=>{setMenu("accessories")}}>Accessories {menu==="accessories"?<hr/>:<></>}</li>
+        {/* Use link to link words to their respective pages */}
+        <li onClick={()=>{setMenu("shop")}}><Link style={{ textDecoration: 'none'}} to='/'>Shop</Link> {menu==="shop"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("bottoms")}}><Link style={{ textDecoration: 'none'}} to='/bottoms'>Bottoms</Link> {menu==="bottoms"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("tops")}}><Link style={{ textDecoration: 'none'}} to='/tops'>Tops</Link> {menu==="tops"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("accessories")}}><Link style={{ textDecoration: 'none'}} to='accessories'>Accessories</Link> {menu==="accessories"?<hr/>:<></>}</li>
       </ul>
       <div className="nav-login-cart">
-        <button>Login</button>
-        <img src={cart_icon} alt="" />
+        <Link to='/login'><button>Login</button></Link>
+        <Link to='/cart'><img src={cart_icon} alt="" /></Link>
         {/* cart counter icon */}
         <div className="nav-cart-count">0</div>
       </div>
