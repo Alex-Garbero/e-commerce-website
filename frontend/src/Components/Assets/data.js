8f86fc10198cfe12c54f81b4dd62c6bac9f405be
alex <144194651+Alex-Garbero@users.noreply.github.com>
2024-04-30 11:30:26 -0700

    Added Item and Popular jsx and css files for product display

---
+++ frontend/src/Components/Assets/data.js
@@ -0,0 +1,41 @@
+import img1 from './img1.png'
+import img2 from './img2.png'
+import img3 from './img3.png'
+import img4 from './img4.png'
+
+let data_product = [
+    {
+        id: 1,
+        name: "Legend Series Lined Athletic Shorts | Hyper Black",
+        category: "bottoms",
+        image: img1,
+        new_price: 40.0,
+        old_price: 50.0,
+    },
+    {
+        id: 2,
+        name: "Legend Series Lined Athletic Shorts | Desert Sand",
+        category: "bottoms",
+        image: img2,
+        new_price: 40.0,
+        old_price: 50.0,
+    },
+    {
+        id: 3,
+        name: "Stealth Series Lined Athletic Shorts | Concrete Grey",
+        category: "bottoms",
+        image: img3,
+        new_price: 40.0,
+        old_price: 50.0,
+    },
+    {
+        id: 4,
+        name: "Summer Sun Tropics Shorts | Tan",
+        category: "bottoms",
+        image: img4,
+        new_price: 40.0,
+        old_price: 50.0,
+    }
+];
+
+export default data_product;
